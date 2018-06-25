4a2634812f44f5e7dcfa438d839ab43202c44ecf
Daniel Etter <etter.dt@gmail.com>
2018-06-25 18:22:15 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,7 +22,7 @@ end
 
 def find_the_cheese(cheddar_cheese)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  cheddar_cheese.find do |cheese|
+  cheddar_cheese.detect do |cheese|
     cheese_types.include?(cheese)
   end
 end
